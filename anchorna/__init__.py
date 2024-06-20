c276ed68fdfde48529aa97698affd8dde3f0fbe6
Tom Eulenfeld <tom.eulenfeld@uni-jena.de>
2024-06-20 16:35:51 +0200

    raise version number

--- anchorna/__init__.py
+++ anchorna/__init__.py
@@ -7,4 +7,4 @@ Find anchors in RNA/DNA sequences
 from anchorna.core import combine, cutout, find_my_anchors
 from anchorna.io import read_anchors, jalview_features
 
-__version__ = '1.0.0'
+__version__ = '1.0.1-dev'
